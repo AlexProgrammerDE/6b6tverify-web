0413e6e21344ac6c451d65fc63ed890e6d3b4638
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-05-18 19:59:25 +0200

    First work

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' js.hcaptcha.com; style-src 'self' 'unsafe-inline'; object-src 'none'; base-uri 'self'; connect-src 'self' https://verifybackend.6b6t.org; font-src 'self'; frame-src 'self' https://newassets.hcaptcha.com; img-src 'self' data:; manifest-src 'self'; media-src 'self'; worker-src 'none';"
+  }
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
